2b81bd618074a59e3c5fcc86cd30fd16e91936f1
Abdulmajid <abdulmajidhmishk@gmail.com>
2025-07-20 22:07:48 +0200

    edited by Ticket 16

--- app.py
+++ app.py
@@ -110,6 +110,23 @@ def delete_topic(id):
     data_manager.write_data(data_file, topics)
     return '', 204
 
+@app.route('/skills/<id>', methods=['PUT'])
+def update_skill(id):
+    data = request.get_json()
+    if not data or 'name' not in data :
+        return jsonify({"error": "name is required"}), 400
+    
+    data_file = os.path.join(os.path.dirname(__file__), 'data', 'skills.json')
+    skills = data_manager.read_data(data_file)
+    
+    for skill in skills:
+        if skill.get("id") == id:
+            skill['name'] = data['name']
+            data_manager.write_data(data_file, skills)
+            return jsonify(skill), 200
+    
+    return jsonify({"error": "Skill not found"}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True) 
